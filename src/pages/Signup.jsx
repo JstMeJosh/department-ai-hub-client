e62394c348d9cc1e99e121ff943717e8194eddd9
J'st Me Josh <155786589+JstMeJosh@users.noreply.github.com>
2026-07-29 01:36:45 +0100

    Handle and log backend errors on signup
    Log backend errors to the console and set error messages.

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -31,6 +31,7 @@ const Signup = () => {
       toast.success(response.data?.message);
       navigate("/login");
     }  catch (error) {
+      console.log(error.response?.data?.errors;)
     const backendErrors = error.response?.data?.errors;
     if (backendErrors && Array.isArray(backendErrors)) {
         setErrors(backendErrors.map((err) => err.message));
